9e6d57f26fc452d909dea78b5d74300fd13dec71
micartey <me@micartey.dev>
2021-09-28 11:02:59 +0200

    feat: add caching

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import Validate from '../../components/Validate';
-import rehypeRaw from 'rehype-raw'
-import ReactMarkdown from 'react-markdown';
-import "./Home.css";
-import { Link } from 'react-router-dom';
 import isElectron from 'is-electron';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Link } from 'react-router-dom';
+import rehypeRaw from 'rehype-raw';
+import Validate from '../../components/Validate';
+import "./Home.css";
 
 function Home() {
 
-    const [content, setContent] = React.useState('');
+    const [content, setContent] = React.useState(localStorage.getItem("readme") || '');
 
     React.useEffect(() => {
         fetch('https://raw.githubusercontent.com/nura-vault/nura-pwa/master/README.md')
             .then((resp) => resp.text())
-            .then((text) => setContent(text))
+            .then((text) => {
+                localStorage.setItem("readme", text);
+                setContent(text);
+            })
     }, []);
 
     const Navbar = () => {
