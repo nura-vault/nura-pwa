2a30831accaf3ea7e62a780cb5a0be220a183e00
micartey <me@micartey.dev>
2021-11-26 19:42:32 +0100

    style(css): remove unused tags

--- src/routes/vault/Audit.tsx
+++ src/routes/vault/Audit.tsx
@@ -4,58 +4,6 @@ import ContainerList from "../../components/ContainerList";
 import { AuditLog, getAudit } from "../../endpoints/Audit";
 import { useSelector } from "../../store/store";
 
-const Container = styled.div`
-    border-radius: 10px;
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25),
-        0 10px 10px rgba(0,0,0,0.22);
-    position: relative;
-    overflow: hidden;
-    width: 768px;
-    max-width: 100%;
-
-    margin-top: 20px;
-    height: calc(100vh - 100px);
-
-    display: flex;
-    justify-content: space-between;
-`
-
-const Parent = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    align-content: center;
-    justify-content: center;
-
-    min-height: 100vh;
-`
-
-const PanelParent = styled.div`
-    width: 100%;
-    padding: 0, 40px;
-    max-height: 100%;
-    overflow-y: scroll;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    align-content: center;
-`
-
-const Panel = styled.div`
-    padding: 0, 40px;
-    max-height: 100%;
-    overflow-y: scroll;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    align-content: center;
-    user-select: text;
-`
-
 function Audit() {
 
     const mail = useSelector(state => state.auth.mail);
@@ -65,6 +13,7 @@ function Audit() {
 
     React.useEffect(() => {
         getAudit(mail, token, audits => {
+            console.log(audits);
             setAudit(audits);
         })
     }, [])
