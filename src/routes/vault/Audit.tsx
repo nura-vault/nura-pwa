4a6c0803a5016eff3dad75278ea9bfb8732f7f2d
micartey <me@micartey.dev>
2022-06-12 20:13:09 +0200

    feat: show token

--- src/routes/vault/Audit.tsx
+++ src/routes/vault/Audit.tsx
@@ -50,12 +50,12 @@ function Audit() {
     }, [])
 
     const Device = (device: string) => {
+        if (device.includes("Android") || device.includes("iPhone") || device.includes("Mobile"))
+            return <Andorid />
         if (device.includes("Windows"))
             return <Windows />
         if (device.includes("Mac"))
             return <Mac />
-        if (device.includes("Android") || device.includes("iOS"))
-            return <Andorid />
         return <Other />
     }
 
@@ -81,27 +81,32 @@ function Audit() {
                 ]
             }
         >
-            <table>
-                {audit?.flatMap(audit => audit.accessToken).map(token => {
-                    return audit.filter(audit => audit.accessToken === token).map(audit => {
-                        return (
-                            <tr>
-                                <td>
-                                    <div>
-                                        {Device(audit.agent)}
-                                    </div>
-                                </td>
-                                <td>
-                                    {audit.agent.match(/\((.*?)\)/)?.[1]} <br />
-                                    {/\w+\s(\w+\/\w+)/g.exec(audit.agent)?.[1]}
-                                </td>
-                                <td> {new Date(audit.timestamp).toLocaleString()} </td>
-                                <td> {audit.message} </td>
-                            </tr>
-                        )
-                    })
-                })}
-            </table>
+
+            {audit?.flatMap(audit => audit.accessToken).map(token => {
+                return (<>
+                    <h3>{token}</h3>
+                    <table>
+                        {audit.filter(audit => audit.accessToken === token).filter(audit => true).map(audit => {
+                            return (
+                                <tr>
+                                    <td>
+                                        <div>
+                                            {Device(audit.agent)}
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {audit.agent.match(/\((.*?)\)/)?.[1]} <br />
+                                        {/\w+\s(\w+\/\w+)/g.exec(audit.agent)?.[1]}
+                                    </td>
+                                    <td> {new Date(audit.timestamp).toLocaleString()} </td>
+                                    <td> {audit.message} </td>
+                                </tr>
+                            )
+                        })}
+                    </table>
+                </>)
+            })}
+
         </ContainerList>
     </>)
 }
